a89faca2aef75b8e5ab982f1c40f95ab9af795d7
Peefy <xpf6677@163.com>
2017-11-16 19:22:50 +0800

    Create files using vs code Signed-off-by: Peefy <xpf6677@163.com>

---
+++ dugu.java
@@ -0,0 +1,15 @@
+//dugu
+import java.lang.System;
+import java.io.*;
+
+public class dugu {
+    public static void main(String[] args) {
+        System.out.println("DuGu, Hello Java in Vs Code");
+        System.out.println("hahahah lalalala");
+        System.out.println(my.ss);
+    }
+}
+
+class my {
+    public static String ss = "aa";
+}
