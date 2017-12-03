7641be3d8c4123a67f9e20b68950b658ac6841cf
Peefy <xpf6677@163.com>
2017-12-03 17:31:34 +0800

    添加了调试Java的插件，调试控制台程序

---
+++ A.java
@@ -0,0 +1,6 @@
+import java.lang.*;
+
+
+public interface myinterface{
+    public void Get();
+}
--- dugu.java
+++ dugu.java
@@ -1,15 +1,32 @@
 //dugu
 import java.lang.System;
+import java.awt.SystemColor;
 import java.io.*;
+import java.lang.System;
+import java.util.*;
+import java.*;
 
 public class dugu {
     public static void main(String[] args) {
         System.out.println("DuGu, Hello Java in Vs Code");
+        Mydugu dugu = new Mydugu();
         System.out.println("hahahah lalalala");
-        System.out.println(my.ss);
+        System.out.println(my.ss.toString());
+        System.out.println(dugu.ok + dugu.num);
+        dugu.Get();
+        System.out.println(dugu.getClass().toString());
     }
 }
 
-class my {
+class my implements myinterface{
     public static String ss = "aa";
+    @Override
+    public void Get(){
+
+    }
+}
+
+class Mydugu extends my implements myinterface{
+    public String ok = "ok";
+    public int num = 2;
 }
